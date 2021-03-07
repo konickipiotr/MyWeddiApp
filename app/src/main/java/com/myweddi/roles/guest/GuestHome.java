e1fd4c6a21089c72185bd8c2b3e6eb167a0d068c
Piotr Konicki <konicki.piotr@gmail.com>
2021-03-07 11:32:47 +0100

    Home page v0.1

--- app/src/main/java/com/myweddi/roles/guest/GuestHome.java
+++ app/src/main/java/com/myweddi/roles/guest/GuestHome.java
@@ -3,6 +3,8 @@ package com.myweddi.roles.guest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.myweddi.R;
 
@@ -10,7 +12,15 @@ public class GuestHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.setting_menu, menu);
+        return true;
     }
 }
