674144191482da83f519217d25d0d6403666b36b
Patrycja <patrycia.puchalska@gmail.com>
2021-03-30 19:23:17 +0200

    some small changes

---
+++ app/src/main/java/com/myweddi/roles/guest/ConfirmActivity.java
@@ -0,0 +1,14 @@
+package com.myweddi.roles.guest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+    }
+}
---
+++ app/src/main/java/com/myweddi/roles/guest/PostActivity.java
@@ -0,0 +1,14 @@
+package com.myweddi.roles.guest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
